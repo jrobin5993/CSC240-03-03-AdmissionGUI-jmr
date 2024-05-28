5c92e31e0d71090b054da322cc7ce8a5d812108c
jrobin5993 <jrobin5993@my.sullivan.edu>
2024-05-28 10:08:08 -0400

    finished creating the appropiate display for the program

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            UxTestScoreTextBox = new TextBox();
+            UxGPATextBox = new TextBox();
+            UxCheckEligibilityButton = new Button();
+            UxResultLabel = new Label();
+            UxTestScoreLabel = new Label();
+            UxGPALabel = new Label();
+            SuspendLayout();
+            // 
+            // UxTestScoreTextBox
+            // 
+            UxTestScoreTextBox.Location = new Point(87, 187);
+            UxTestScoreTextBox.Name = "UxTestScoreTextBox";
+            UxTestScoreTextBox.Size = new Size(150, 31);
+            UxTestScoreTextBox.TabIndex = 0;
+            // 
+            // UxGPATextBox
+            // 
+            UxGPATextBox.Location = new Point(87, 347);
+            UxGPATextBox.Name = "UxGPATextBox";
+            UxGPATextBox.Size = new Size(150, 31);
+            UxGPATextBox.TabIndex = 1;
+            // 
+            // UxCheckEligibilityButton
+            // 
+            UxCheckEligibilityButton.Location = new Point(282, 254);
+            UxCheckEligibilityButton.Name = "UxCheckEligibilityButton";
+            UxCheckEligibilityButton.Size = new Size(106, 61);
+            UxCheckEligibilityButton.TabIndex = 2;
+            UxCheckEligibilityButton.Text = "Check Eligibility";
+            UxCheckEligibilityButton.UseVisualStyleBackColor = true;
+            // 
+            // UxResultLabel
+            // 
+            UxResultLabel.AutoSize = true;
+            UxResultLabel.Location = new Point(523, 139);
+            UxResultLabel.Name = "UxResultLabel";
+            UxResultLabel.Size = new Size(63, 25);
+            UxResultLabel.TabIndex = 3;
+            UxResultLabel.Text = "Result:";
+            // 
+            // UxTestScoreLabel
+            // 
+            UxTestScoreLabel.AutoSize = true;
+            UxTestScoreLabel.Location = new Point(87, 139);
+            UxTestScoreLabel.Name = "UxTestScoreLabel";
+            UxTestScoreLabel.Size = new Size(95, 25);
+            UxTestScoreLabel.TabIndex = 4;
+            UxTestScoreLabel.Text = "Test Score:";
+            // 
+            // UxGPALabel
+            // 
+            UxGPALabel.AutoSize = true;
+            UxGPALabel.Location = new Point(92, 304);
+            UxGPALabel.Name = "UxGPALabel";
+            UxGPALabel.Size = new Size(49, 25);
+            UxGPALabel.TabIndex = 5;
+            UxGPALabel.Text = "GPA:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(UxGPALabel);
+            Controls.Add(UxTestScoreLabel);
+            Controls.Add(UxResultLabel);
+            Controls.Add(UxCheckEligibilityButton);
+            Controls.Add(UxGPATextBox);
+            Controls.Add(UxTestScoreTextBox);
+            Name = "Form1";
+            RightToLeftLayout = true;
+            Text = "College Admissions";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox UxTestScoreTextBox;
+        private TextBox UxGPATextBox;
+        private Button UxCheckEligibilityButton;
+        private Label UxResultLabel;
+        private Label UxTestScoreLabel;
+        private Label UxGPALabel;
     }
 }
